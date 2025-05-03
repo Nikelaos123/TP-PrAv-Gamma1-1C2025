c61e74a73b8b376cca1a67b7173aecc65a11fe09
wguardia94 <willianguardia@gmail.com>
2025-05-03 16:32:58 -0300

    Clase imagen con escalamiento y guardado funcionando

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Testing");
+    public static void main(String[] args) throws IOException {
+        Imagen im1 = new Imagen(".\\im\\1.pgm");
+
+        im1.guardarImg(im1.cambiarEscala(3));
     }
 
 }
