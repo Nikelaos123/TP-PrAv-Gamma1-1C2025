2203e0f1ea80b164a499cdf61301dc64512dace7
Nicolas <nicodjan200126@gmail.com>
2025-05-03 18:27:28 -0300

    idem

--- src/App.java
+++ src/App.java
@@ -9,15 +9,23 @@ public class App {
 
 
         /*
-        import java.io.FileNotFoundException;
-        import java.io.IOException;
+        System.out.println("Matriz Original: ");
+        int[][] original = {
+            {1, 5},
+            {5, 1}
+        };
+        Matriz m1 = new Matriz(original);
 
-        public class App {
+        m1.mostrarMatriz();
 
-        public static void main(String[] args) throws IOException {
-            Imagen im1 = new Imagen(".\\im\\1.pgm");
+        int valorEscalado = 3;
+        System.out.println();
 
-        im1.guardarImg(im1.cambiarEscala(3));
+        System.out.println("Matriz Escalada * "+ valorEscalado +": ");
+        
+
+        Matriz m2 = new Matriz(m1.retornarEscalada(valorEscalado));
+        m2.mostrarMatriz();
 
         */
     }
