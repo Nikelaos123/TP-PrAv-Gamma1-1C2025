1cb9de7eb90f442a07ba704bef7e848ae8104418
Nicolas <nicodjan200126@gmail.com>
2025-05-03 18:13:39 -0300

    formulas de escalado base

--- src/App.java
+++ src/App.java
@@ -2,7 +2,26 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Testing");
+        
+        System.out.println("Matriz Original: ");
+        int[][] original = {
+            {1, 5},
+            {5, 1}
+        };
+        Matriz m1 = new Matriz(original);
+
+        m1.mostrarMatriz();
+
+        int valorEscalado = 3;
+        System.out.println();
+
+        System.out.println("Matriz Escalada * "+ valorEscalado +": ");
+        
+
+        Matriz m2 = new Matriz(m1.retornarEscalada(valorEscalado));
+        m2.mostrarMatriz();
+
+
     }
 
 }
