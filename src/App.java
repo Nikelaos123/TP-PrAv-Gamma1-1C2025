a7ae475ae4052e8dd25dfc7de1bf8707f6c7d4b1
Nicolas <nicodjan200126@gmail.com>
2025-05-03 18:23:22 -0300

    resolve app

--- src/App.java
+++ src/App.java
@@ -1,31 +1,25 @@
+import java.io.IOException;
 
 public class App {
-
-    public static void main(String[] args) {
-        
-        System.out.println("Matriz Original: ");
-        int[][] original = {
-            {1, 5},
-            {5, 1}
-        };
-        Matriz m1 = new Matriz(original);
-
-        m1.mostrarMatriz();
-
-        int valorEscalado = 3;
-        System.out.println();
-
-        System.out.println("Matriz Escalada * "+ valorEscalado +": ");
-        
-
-        Matriz m2 = new Matriz(m1.retornarEscalada(valorEscalado));
-        m2.mostrarMatriz();
-
 
     public static void main(String[] args) throws IOException {
         Imagen im1 = new Imagen(".\\im\\1.pgm");
 
         im1.guardarImg(im1.cambiarEscala(3));
+
+
+        /*
+        import java.io.FileNotFoundException;
+        import java.io.IOException;
+
+        public class App {
+
+        public static void main(String[] args) throws IOException {
+            Imagen im1 = new Imagen(".\\im\\1.pgm");
+
+        im1.guardarImg(im1.cambiarEscala(3));
+
+        */
     }
 
 }
